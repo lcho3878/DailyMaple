1abb383952455fd26f24bd53425c6d7414144667
lcho3878 <leech3878@naver.com>
2024-09-21 21:11:53 +0900

    Initial Commit

---
+++ DailyMaple/DailyMapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMapleApp.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyMapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
