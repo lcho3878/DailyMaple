ec2ef0a1aa5b549f8f0929b6d42234f89df927d6
lcho3878 <leech3878@naver.com>
2024-09-21 21:37:28 +0900

    Feat: Create CharacterResponse Model

---
+++ DailyMaple/CharacterResponse.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterResponse.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/21/24.
+//
+
+import Foundation
+
+struct CharacterResponse: Decodable {
+    let date: String
+    let character_name: String
+    let world_name: String
+    let character_gender: String
+    let character_class: String
+    let character_class_level: String
+    let character_level: Int
+    let character_exp: Int
+    let character_exp_rate: String
+    let character_guild_name: String
+    let character_image: String
+    let liberation_quest_clear_flag: String
+}
