5856ba9d49df222090d4fae71c8b43cb9b24d05a
lcho3878 <leech3878@naver.com>
2024-09-22 16:19:44 +0900

    Feat: Create SymbolResponseModel

---
+++ DailyMaple/SymbolResponseModel.swift
@@ -0,0 +1,27 @@
+//
+//  SymbolResponseModel.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/22/24.
+//
+
+import Foundation
+
+struct SymbolResponseModel: Decodable {
+    let symbol: [Symbol]
+    
+    struct Symbol: Decodable {
+        let symbol_name: String
+        let symbol_icon: String
+        let symbol_description: String
+        let symbol_force: String
+        let symbol_level: Int
+        let symbol_str: String
+        let symbol_dex: String
+        let symbol_int: String
+        let symbol_luk: String
+        let symbol_hp: String
+        let symbol_growth_count: Int
+        let symbol_require_growth_count: Int
+    }
+}
