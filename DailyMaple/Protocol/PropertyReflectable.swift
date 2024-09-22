4385d8ecd49ea20988e9c7e9b755df0b54188d4b
lcho3878 <leech3878@naver.com>
2024-09-22 13:16:47 +0900

    Feat: Create PropertyReflectable Protocol

--- DailyMaple/Protocol/PropertyReflectable.swift
+++ DailyMaple/Protocol/PropertyReflectable.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol PropertyReflectable {}
+
+extension PropertyReflectable {
+    subscript(key: String) -> Any? {
+        let m = Mirror(reflecting: self)
+        for child in m.children {
+            if child.label == key {
+                return child.value
+            }
+        }
+        return nil
+    }
+}
