5048699930437712ee00a0798c80f77a34a73954
lcho3878 <leech3878@naver.com>
2024-09-23 17:43:56 +0900

    Add: Create MainTabView

---
+++ DailyMaple/Presentation/MainTapView/MainTabView.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabView.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/23/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            CharacterInfoView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("캐릭터 정보")
+                }
+            VStack {
+                Text("Event View")
+            }
+            .tabItem {
+                Image(systemName: "calendar")
+                Text("이벤트")
+            }
+            
+            VStack {
+                Text("QuestView")
+            }
+            .tabItem {
+                Image(systemName: "note.text")
+                Text("메할일")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
