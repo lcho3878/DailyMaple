344a3832201b0f2b696338f401cce2162a44b73f
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 01:47:11 +0900

    Feat: Create CashNoticesResponseModel

---
+++ DailyMaple/CashNoticesResponseModel.swift
@@ -0,0 +1,22 @@
+//
+//  CashNoticesResponseModel.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/27/24.
+//
+
+import Foundation
+
+struct CashNoticesResponseModel: Decodable {
+    let cashshop_notice: [CashNotice]
+    
+    struct CashNotice: Decodable {
+        let title: String
+        let url: String
+        let notice_id: Int
+        let date: String
+        let date_sale_start: String
+        let date_sale_end: String
+        let ongoing_flag: Bool
+    }
+}
