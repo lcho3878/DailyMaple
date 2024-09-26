75184c0602992b18c74aeb84a6ebc5f534891171
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 04:18:39 +0900

    Feat: Create String Extension formatNumberString function

--- DailyMaple/Extension/String+.swift
+++ DailyMaple/Extension/String+.swift
@@ -9,10 +9,18 @@ import Foundation
 
 extension String {
     static let formatter = DateFormatter()
+    static let numberFormatter = NumberFormatter()
     
     public func toDateString(format: String) -> String {
         String.formatter.dateFormat = "yyyy-MM-dd'T'HH:mmXXXXX"
         guard let date = String.formatter.date(from: self) else { return "닐인데요ㅕ" }
         return date.asString(format: format)
+    }
+    
+    public func formatNumberString() -> String {
+        guard let number = Int(self) else { return "" }
+        String.numberFormatter.numberStyle = .decimal
+        guard let formattedNumber = String.numberFormatter.string(from: NSNumber(value: number)) else { return "" }
+        return formattedNumber
     }
 }
