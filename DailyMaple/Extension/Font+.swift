844e146386cd1a44c5054ac11c47dbdd24314b3b
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 03:59:04 +0900

    Feat: Add Font Case

--- DailyMaple/Extension/Font+.swift
+++ DailyMaple/Extension/Font+.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 extension Font {
+    static let mapleBold20: Font = .custom("Maplestory OTF Bold", size: 20)
+    static let mapleBold18: Font = .custom("Maplestory OTF Bold", size: 18)
     static let mapleBold16: Font = .custom("Maplestory OTF Bold", size: 16)
     static let mapleBold14: Font = .custom("Maplestory OTF Bold", size: 14)
+    static let mapleLight20: Font = .custom("Maplestory OTF Light", size: 20)
+    static let mapleLight18: Font = .custom("Maplestory OTF Light", size: 18)
     static let mapleLight16: Font = .custom("Maplestory OTF Light", size: 16)
     static let mapleLight14: Font = .custom("Maplestory OTF Light", size: 14)
 }
