a3598808e9128e29882ea5bfce381cc7337014c1
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 03:26:44 +0900

    Feat: Implement EventView, ViewModel

--- DailyMaple/Presentation/MainTapView/EventView/EventView.swift
+++ DailyMaple/Presentation/MainTapView/EventView/EventView.swift
@@ -10,23 +10,56 @@ import SwiftUI
 struct EventView: View {
     typealias Event = EventsResponseModel.Event
     
-    @State private var viewModel = EventViewModel()
+    @StateObject private var viewModel = EventViewModel()
     
     var body: some View {
         NavigationView {
-            List {
-                Section {
-                    ForEach(viewModel.output.events, id: \.notice_id) { event in
-                        NavigationLink {
-                            WebView(urlToLoad: event.mobileURL)
-                        } label: {
-                            VStack(alignment:. leading, spacing: 10) {
-                                Text(event.title)
-                                    .font(.mapleBold16)
-                                Text("\(event.startDate) ~ \(event.endDate)")
-                                    .font(.mapleLight14)
+            VStack {
+                Picker("Menu", selection: $viewModel.output.pickerOutput) {
+                    ForEach(EventViewModel.TapMenu.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                List {
+                    Section {
+                        switch viewModel.output.pickerOutput {
+                        case .notices:
+                            EventListView(data: viewModel.output.notices)
+                        case .updates:
+                            EventListView(data: viewModel.output.updates)
+                        case .events:
+                            EventListView(data: viewModel.output.events)
+                        case .cashNotices:
+                            EventListView(data: viewModel.output.cashNotices)
+                        }
+                    } header: {
+                        Text(viewModel.output.pickerOutput.rawValue)
+                    }
+
+                }
+            }
+        }
+    }
+    
+    struct EventListView: View {
+        let data: [Eventable]
+        var body: some View {
+            ForEach(data, id: \.notice_id) { event in
+                NavigationLink {
+                    WebView(urlToLoad: event.mobileURL)
+                } label: {
+                    VStack(alignment:. leading, spacing: 10) {
+                        Text(event.title)
+                            .font(.mapleBold16)
+                        HStack {
+                            Text(event.startDate)
+                            if let endDate = event.endDate {
+                                Text(" ~ \(endDate)")
                             }
                         }
+                        .font(.mapleLight14)
                     }
                 }
             }
--- DailyMaple/Presentation/MainTapView/EventView/EventViewModel.swift
+++ DailyMaple/Presentation/MainTapView/EventView/EventViewModel.swift
@@ -6,16 +6,44 @@
 //
 
 import Foundation
+import Combine
 
 final class EventViewModel: ObservableObject {
     
+    var input = Input()
     @Published var output = Output()
     
+    struct Input {
+
+    }
+    
     struct Output {
-        var events: [EventsResponseModel.Event] = []
+        var pickerOutput: TapMenu = .notices
+        var notices: [Eventable] = []
+        var updates: [Eventable] = []
+        var events: [Eventable] = []
+        var cashNotices: [Eventable] = []
     }
     
     init() {
+        loadEvents()
+        loadNotices()
+        loadUpdates()
+        loadCashNotices()
+    }
+}
+
+extension EventViewModel {
+    enum TapMenu: String, CaseIterable {
+        case notices = "공지사항"
+        case updates = "업데이트"
+        case events = "이벤트"
+        case cashNotices = "캐시샵 공지"
+    }
+}
+
+extension EventViewModel {
+    private func loadEvents() {
         guard let data = MockDataManager.shared.loadData(fileName: "Events") else { return }
         do {
             let result = try JSONDecoder().decode(EventsResponseModel.self, from: data)
@@ -23,7 +51,43 @@ final class EventViewModel: ObservableObject {
             print("Events Data Load")
         }
         catch {
-            print("Evnet Error")
+            print("Event Error")
+        }
+    }
+    
+    private func loadNotices() {
+        guard let data = MockDataManager.shared.loadData(fileName: "Notices") else { return }
+        do {
+            let result = try JSONDecoder().decode(NoticesResponseModel.self, from: data)
+            output.notices = result.notice
+            print("Notices Data Load")
+        }
+        catch {
+            print("Notices Error")
+        }
+    }
+    
+    private func loadUpdates() {
+        guard let data = MockDataManager.shared.loadData(fileName: "Updates") else { return }
+        do {
+            let result = try JSONDecoder().decode(UpdatesResponseModel.self, from: data)
+            output.updates = result.update_notice
+            print("Updates Data Load")
+        }
+        catch {
+            print("Updates Error")
+        }
+    }
+    
+    private func loadCashNotices() {
+        guard let data = MockDataManager.shared.loadData(fileName: "Cash") else { return }
+        do {
+            let result = try JSONDecoder().decode(CashNoticesResponseModel.self, from: data)
+            output.cashNotices = result.cashshop_notice
+            print("CashNotices Data Load")
+        }
+        catch {
+            print("CashNotices Error\n\(error)")
         }
     }
 }
