676966e6e85a649d9c6bdc994307cd896a7f7775
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 16:14:16 +0900

    Feat: Implement CharacterStatView UI

--- DailyMaple/Presentation/CharacterInfoView/CharacterInfoTap/CharacterStatView/CharacterStatView.swift
+++ DailyMaple/Presentation/CharacterInfoView/CharacterInfoTap/CharacterStatView/CharacterStatView.swift
@@ -16,33 +16,74 @@ struct CharacterStatView: View {
             if let stats = viewModel.output.stats {
                 VStack {
                     VStack(spacing: 5) {
-                        Text("전투력 \(stats.statValue(name: "전투력"))")
-                        Text("스탯 공격력 \(stats.statValue(name: "최소 스탯공격력")) ~ \(stats.statValue(name: "최대 스탯공격력"))")
+                        PowerView(power: stats.statValue(name: "전투력"))
+//                        Text("스탯 공격력 \(stats.statValue(name: "최소 스탯공격력")) ~ \(stats.statValue(name: "최대 스탯공격력"))")
                     }
                     .font(.mapleBold(20))
-                    let rows = [
-                        GridItem(.flexible(minimum: 20, maximum: 100)),
-                        GridItem(.flexible(minimum: 20, maximum: 100)),
+                    let columns = [
+                        GridItem(.flexible(minimum: 50, maximum: .infinity)),
+                        GridItem(.flexible(minimum: 50, maximum: .infinity)),
+                       
                     ]
-                    LazyHGrid(rows: rows) {
+                    LazyVGrid(columns: columns) {
                         ForEach(Stat.MainStat.allCases, id: \.self) { item in
-                            HStack {
-                                Text(item.rawValue)
-                                Text(stats.statValue(name: item.rawValue))
-                            }
+                            MainStatView(name: item.rawValue, value: stats.statValue(name: item.rawValue))
                         }
                         .padding()
                     }
+                    .frame(maxWidth: .infinity)
+                    .background(Color(hex: 0x8694a0))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
                     VStack {
                         StatRowView(data: Stat.DamageStat.allCases, stats: stats)
                         StatRowView(data: Stat.SubStat.allCases, stats: stats)
-                        StatRowView(data: Stat.ForceStat.allCases, stats: stats)
+                        
                         StatRowView(data: Stat.CoolTimeStat.allCases, stats: stats)
                         StatRowView(data: Stat.AttributeStat.allCases, stats: stats)
+                        StatRowView(data: Stat.ForceStat.allCases, stats: stats)
                         StatRowView(data: Stat.OtherStat.allCases, stats: stats)
                     }
+                    .background(Color(hex: 0x6c7885))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
                 }
+                .background(Color(hex: 0x28323b))
                 .font(.mapleBold(16))
+            }
+        }
+    }
+    
+    struct PowerView: View {
+        let power: String
+        var body: some View {
+            ZStack(alignment: .center) {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(hex: 0x3f6076))
+                    .frame(height: 50)
+                    HStack {
+                        Text("전투력")
+                            .foregroundStyle(Color(hex: 0xd4eef2))
+                        Spacer()
+                        Text(power)
+                            .foregroundStyle(Color(hex: 0xfffad2))
+                        Spacer()
+                        Spacer()
+                    }
+                    .padding()
+            }
+        }
+    }
+    
+    struct MainStatView: View {
+        let name: String
+        let value: String
+        var body: some View {
+            HStack{
+                Text(name)
+                    .foregroundStyle(Color(hex: 0xdce5ec))
+                    .shadow(color: .black, radius: 1, x: 1, y: 1)
+                Spacer()
+                Text(value)
+                    .foregroundStyle(Color(hex: 0xe5eef4))
             }
         }
     }
@@ -54,11 +95,16 @@ struct CharacterStatView: View {
             ForEach(data, id: \.rawValue) { item in
                 HStack {
                     Text(item.rawValue)
+                        .foregroundStyle(Color(hex: 0xcad6db))
+                        .shadow(color: .black, radius: 1, x: 1, y: 1)
                     Spacer()
-                    Text(stats.statValue(name: item.rawValue))
-                    if let tailString = item.tailString {
-                        Text(tailString)
+                    HStack(spacing: 0) {
+                        Text(stats.statValue(name: item.rawValue))
+                        if let tailString = item.tailString {
+                            Text(tailString)
+                        }
                     }
+                    .foregroundColor(.white)
                 }
             }
             .padding()
