8b30e6e43d03ee28a7e6c4bfd2ac59ca9a5a59a2
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-27 18:38:53 +0900

    Feat: Implement HyperStatView UI

--- DailyMaple/Presentation/CharacterInfoView/CharacterInfoTap/HyperStatView/HyperStatView.swift
+++ DailyMaple/Presentation/CharacterInfoView/CharacterInfoTap/HyperStatView/HyperStatView.swift
@@ -13,67 +13,122 @@ struct HyperStatView: View {
     
     var body: some View {
         ScrollView {
-            Text("어빌리티")
-                .padding(.bottom)
-                .font(.mapleBold(16))
-            HyperPickerView(input: viewModel.input.abilityPickerInput, output: $viewModel.output.abilityPickerOutput)
             VStack {
-                Text(viewModel.output.ability.ability_preset_grade)
-                    .padding(.bottom, 5)
+                Text("어빌리티")
+                    .padding(.bottom)
+                    .foregroundColor(.legendary)
+                    .font(.mapleBold(16))
+                    PickerView(input: viewModel.input.abilityPickerInput, output: $viewModel.output.abilityPickerOutput)
+                    AbilityContentView(viewModel: viewModel)
+                
+            }
+            .padding(.vertical)
+            .background(Color.statBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding(.vertical)
+            
+            HyperStatContentView(viewModel: viewModel)
+        }
+        .background(Color.infoBackground)
+    }
+    
+    struct AbilityContentView: View {
+        @StateObject var viewModel: HyperStatViewModel
+        var body: some View {
+            VStack {
+                HStack {
+                    Image(systemName: "bookmark.fill")
+                    Text("\(viewModel.output.ability.ability_preset_grade) 어빌리티")
+                }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .foregroundStyle(.white)
+                    .background(viewModel.output.ability.abilityColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding(.horizontal)
                 VStack(alignment: .leading) {
                     ForEach(viewModel.output.ability.ability_info, id: \.ability_no) { ability in
                         Text(ability.ability_value)
-                            .foregroundStyle(ability.abilityColor)
-                            
-                            .padding(.top, 5)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 10)
+                            .foregroundStyle(.white)
+                            .background(ability.abilityColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .padding(.horizontal)
                     }
                 }
-            }
-            .padding(.vertical, 10)
-            .font(.mapleLight(16))
-            
-            Text("하이퍼스탯")
                 .padding(.vertical)
-                .font(.mapleBold(16))
-            HyperPickerView(input: viewModel.input.hyperPickerInput, output: $viewModel.output.hyperPickerOutput)
-                .padding(.bottom, 5)
-            VStack(alignment: .leading) {
-                ForEach(viewModel.output.hyperStats, id: \.stat_type) { item in
-                    if let statIncrease = item.stat_increase {
-                        HStack {
-                            Text("Lv.\(item.stat_level)")
-                            Text(statIncrease)
-                        }
-                        .font(.mapleLight(14))
-                        .padding(.bottom, 5)
-                        
-                    }
-                }
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .padding(.horizontal)
             }
-            .padding(.vertical, 10)
+            
+            .font(.mapleLight(16))
         }
     }
     
-    struct HyperPickerView: View {
-//        var input: HyperStatViewModel.Input
+    struct HyperStatContentView: View {
+        @StateObject var viewModel: HyperStatViewModel
+        var body: some View {
+            VStack {
+                Text("하이퍼스텟")
+                    .padding(.bottom)
+                    .foregroundColor(.legendary)
+                    .font(.mapleBold(16))
+                PickerView(input: viewModel.input.hyperPickerInput, output: $viewModel.output.hyperPickerOutput)
+                    .padding(.bottom, 5)
+                VStack(alignment: .leading) {
+                    ForEach(viewModel.output.hyperStats, id: \.stat_type) { item in
+                        if let statIncrease = item.stat_increase {
+                            HStack {
+                                Text(statIncrease)
+                                    .foregroundStyle(Color(hex: 0xcad6db))
+                                    .shadow(color: .black, radius: 1, x: 1, y: 1)
+                                Spacer()
+    //                            HStack(spacing: 0) {
+                                    Text("Lv. \(item.stat_level)")
+    //                            }
+                                .foregroundColor(.white)
+                            }
+                            .font(.mapleBold(16))
+                            .padding()
+                        }
+                    }
+                }
+                .padding(.vertical, 10)
+            }
+            .padding(.vertical)
+            .background(Color.statBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+    }
+    
+    struct PickerView: View {
         var input: PassthroughSubject<Int, Never>
         @Binding var output: Int
         
         var body: some View {
             HStack {
+                Text("PRESETS")
                 ForEach(1..<4) { num in
                     Button(action: {
                         input.send(num)
                     }, label: {
-                        Text("프리셋 \(num)")
-                            .foregroundStyle(Color(uiColor: output == num ? .systemBackground : .label))
-                            .background(Color(uiColor: output == num ? .label : .systemBackground))
+                        Text("\(num)")
+                            .frame(width: 20, height: 20)
+                            .foregroundStyle(.white)
+                            .background(output == num ? .black : Color.infoBackground)
+                            .border(output == num ? .white : .black, width: 1)
                             .font(.mapleBold(16))
                     })
                 }
             }
+            .padding(5)
+            .background(Color(hex: 0x646f7c))
+            .clipShape(RoundedRectangle(cornerRadius: 8))
         }
     }
+    
 }
 
 #Preview {
