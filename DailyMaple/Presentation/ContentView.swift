bd97b77cbb46de0f6aabbaa68e22875fd27f8e89
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-29 19:53:31 +0900

    Feat: Create OnboardingViews and Temporary branch handling

--- DailyMaple/Presentation/ContentView.swift
+++ DailyMaple/Presentation/ContentView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        MainTabView()
+        if let ocid = UserDefaults.standard.string(forKey: "ocid") {
+            MainTabView()
+        }
+        else if let apiKey = UserDefaults.standard.string(forKey: "apiKey") {
+            OcidCheckView()
+        }
+        else {
+            APIKeyView()
+        }
     }
 }
 
---
+++ DailyMaple/Presentation/OnboardingView/OcidCheckView.swift
@@ -0,0 +1,18 @@
+//
+//  OcidCheckView.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/29/24.
+//
+
+import SwiftUI
+
+struct OcidCheckView: View {
+    var body: some View {
+        Text("Ocid CheckView")
+    }
+}
+
+#Preview {
+    OcidCheckView()
+}
