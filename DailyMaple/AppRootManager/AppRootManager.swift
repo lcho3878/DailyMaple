4a5445606fef03b5c3e00322a324c4bc34f66d0c
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-29 23:10:19 +0900

    Feat: Create AppRootManager

---
+++ DailyMaple/AppRootManager/AppRootManager.swift
@@ -0,0 +1,40 @@
+//
+//  AppRootManager.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+final class AppRootManager: ObservableObject {
+
+    @UserDefault(key: "ocid", defaultValue: nil, storage: .standard)
+    var ocid: String?
+    
+    @UserDefault(key: "apikey", defaultValue: nil, storage: .standard)
+    var apikey: String?
+    
+    @Published var currentRoot: AppRoot = .api
+    
+    init() {
+        if apikey == nil {
+            currentRoot = .api
+        }
+        else if ocid == nil {
+            currentRoot = .ocid
+        }
+        else {
+            currentRoot = .main
+        }
+    }
+}
+
+extension AppRootManager {
+    enum AppRoot {
+        case api
+        case ocid
+        case main
+    }
+}
