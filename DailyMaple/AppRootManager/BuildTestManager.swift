5dbcd9ad873bd25e4ae1073dee74ed8bef80d8d5
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-09-30 00:58:55 +0900

    Feat: Create BuildTestManager & UserDefaultManager

---
+++ DailyMaple/AppRootManager/BuildTestManager.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTestManager.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/30/24.
+//
+
+import Foundation
+
+final class BuildTestManager {
+    static let shared = BuildTestManager()
+    
+    let isNetworking = true
+    
+    private init() {}
+
+}
---
+++ DailyMaple/PropertyWrapper/UserDefaultManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultManager.swift
+//  DailyMaple
+//
+//  Created by 이찬호 on 9/30/24.
+//
+
+import Foundation
+
+struct UserDefaultManager {
+    @UserDefault(key: "ocid", defaultValue: nil, storage: .standard)
+    static var ocid: String?
+}
